3374d45740bbb8c264e3f14cc152e8915bbda651
Dan <35927536+dan-mba@users.noreply.github.com>
2023-06-19 07:22:10 -0400

    fix eslint for typescript (#300)

--- routes/get.ts
+++ routes/get.ts
@@ -2,29 +2,31 @@ import { fetch } from 'undici';
 import { parseStringPromise } from 'xml2js';
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
-type JSONResponse = {data: string};
-
-function get (fastify: FastifyInstance, opts: FastifyPluginOptions, done: Function) {
+function get (fastify: FastifyInstance, opts: FastifyPluginOptions, done: (err?: Error) => void) {
   fastify.get<{
     Querystring: {
       rss: string
     }
-  }>('/', async function (request) {
+  }>('/', async function (request, reply) {
     if (!request.query.rss) {
+      reply.type('application/json').code(400);
       return { error: 'No rss parameter specified' };
     } else {
       try {
         const res = await fetch(request.query.rss);
         const data = await res.text();
-        const rss = await parseStringPromise(data, { explicitArray: false })
+        const rss = await parseStringPromise(data, { explicitArray: false });
+
+        reply.type('application/json').code(200);
         return rss;
       } catch (error) {
           console.log(error);
+          reply.type('application/json').code(400);
           return {error};
       }
     }
   });
   done();
-};
+}
 
 export default get;
